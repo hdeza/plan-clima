2bbfb2dd539ea25fedc1a085bf507e8f7be58794
LeyLopez <leidislopez2017@gmail.com>
2025-05-19 14:18:18 -0500

    updates

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -19,7 +19,7 @@ export const Login = () => {
   return (
     <>
       <Header isInHome={false} />
-      <div className="flex flex-col items-center justify-center h-svh bg-cover bg-no-repeat bg-center bg-[url('src/assets/Cartagena_login.webp')]">
+      <div className="flex flex-col items-center justify-center h-svh bg-cover bg-no-repeat bg-center bg-[url('src/assets/Cartagena_login.jpg')]">
         <div className="flex-1 flex items-center">
           <div className="w-full max-w-lg">
             <div className="mb-4 ">
