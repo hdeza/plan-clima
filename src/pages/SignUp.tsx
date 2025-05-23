ec6beca591b859af46875532cc7eb6af0fbd6b1b
hdeza <armandohdeza.03@gmail.com>
2025-05-23 08:56:46 -0500

    Refactor Login and SignUp components: Added password visibility toggle, improved error handling during authentication, and enhanced UI layout for better user experience.

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -2,21 +2,24 @@ import { AuthContext } from "@/contexts/AuthProvider";
 import Footer from "@/layouts/Footer";
 import Header from "@/layouts/Header";
 import { FormEvent, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export interface UserRegistration {
-  email: string
-  password: string
-  firstName: string
-  lastname: string
+  email: string;
+  password: string;
+  firstName: string;
+  lastname: string;
 }
 
 export const SignUp = () => {
   const authContext = useContext(AuthContext);
-  const [ confirmPassword, setConfirmPassword ] = useState("");
-  const [ username, setUsername ] = useState("");
+  const navigate = useNavigate();
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const formData = new FormData(e.currentTarget);
@@ -25,37 +28,46 @@ export const SignUp = () => {
       firstName: formData.get("firstName") as string,
       lastname: formData.get("lastName") as string,
       email: formData.get("email") as string,
-      password: formData.get("password") as string
-    }
+      password: formData.get("password") as string,
+    };
 
     if (userData.password !== confirmPassword) {
       alert("Passwords do not match");
-      return
+      return;
     }
 
     if (authContext && authContext.register) {
-      authContext.register(userData.email, userData.password, userData.firstName, userData.lastname);
-      console.log("Registering user.");
+      try {
+        await authContext.register(
+          userData.email,
+          userData.password,
+          userData.firstName,
+          userData.lastname
+        );
+        console.log("Registration successful");
+        navigate("/login"); // Navigate to login page after successful registration
+      } catch (error) {
+        console.error("Registration failed:", error);
+        alert("Registration failed. Please try again.");
+      }
     } else {
       console.error("AuthContext or register function is not available.");
     }
-
-    // Lógica de registro aquí
-    console.log("Registering user.");
-  }
-
+  };
 
   return (
     <>
       <Header isInHome={false} />
       <div className="flex flex-col items-center justify-center h-svh bg-cover bg-no-repeat bg-center bg-[url('src/assets/guatape.jpg')]">
-        <div className="flex flex-1 items-center ">
+        <div className="flex items-center flex-1 ">
           <div className="w-full max-w-lg">
             <div className="mb-5">
-              <h2 className="text-4xl font-serif font-bold text-orange-500">REGISTER</h2>
+              <h2 className="font-serif text-4xl font-bold text-orange-500">
+                REGISTER
+              </h2>
             </div>
 
-            <div className="bg-black/40  backdrop-blur-sm p-8 rounded-lg border-black/25 border-2 shadow-lg">
+            <div className="p-8 border-2 rounded-lg shadow-lg bg-black/40 backdrop-blur-sm border-black/25">
               <form onSubmit={handleSubmit} className="grid grid-cols-6 gap-4">
                 <div className="col-span-3 col-start-1">
                   <label htmlFor="firstName" className="text-white md-text-lg">
@@ -63,10 +75,10 @@ export const SignUp = () => {
                   </label>
                   <input
                     id="firstName"
-                    name="firstName"    
+                    name="firstName"
                     type="text"
                     placeholder="Enter your first name here"
-                    className="bg-white/90 rounded-sm w-full" 
+                    className="w-full rounded-sm bg-white/90"
                     required
                   />
                 </div>
@@ -79,7 +91,7 @@ export const SignUp = () => {
                     name="lastname"
                     type="text"
                     placeholder="Enter your lastname here"
-                    className="bg-white/90 rounded-sm w-full"
+                    className="w-full rounded-sm bg-white/90"
                     required
                   />
                 </div>
@@ -94,7 +106,7 @@ export const SignUp = () => {
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
                     placeholder="Enter your username here"
-                    className="bg-white/90 rounded-sm w-full" 
+                    className="w-full rounded-sm bg-white/90"
                     required
                   />
                 </div>
@@ -107,7 +119,7 @@ export const SignUp = () => {
                     type="email"
                     name="email"
                     placeholder="Enter your e-mail here"
-                    className="bg-white/90 rounded-sm w-full"
+                    className="w-full rounded-sm bg-white/90"
                     required
                   />
                 </div>
@@ -116,46 +128,65 @@ export const SignUp = () => {
                   <label htmlFor="password" className="text-white">
                     Password
                   </label>
-                  <input
-                    id="password"
-                    type="password"
-                    name="password"
-                    placeholder="••••••••••••••"
-                    className="bg-white/90 rounded-sm w-full"
-                    required
-                  />
+                  <div className="relative">
+                    <input
+                      id="password"
+                      type={showPassword ? "text" : "password"}
+                      name="password"
+                      placeholder="••••••••••••••"
+                      className="w-full pr-10 rounded-sm bg-white/90"
+                      required
+                    />
+                    <button
+                      type="button"
+                      onClick={() => setShowPassword(!showPassword)}
+                      className="absolute text-gray-600 -translate-y-1/2 right-2 top-1/2 hover:text-gray-800"
+                    >
+                      {showPassword ? "👁️" : "👁️‍🗨️"}
+                    </button>
+                  </div>
                 </div>
                 <div className="col-span-3 ">
                   <label htmlFor="confirmPassword" className="text-white">
                     Confirm password
                   </label>
-                  <input
-                    id="confirmPassword"
-                    type="password"
-                    name="confirmPassword"
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                    placeholder="••••••••••••••"
-                    className="bg-white/90 rounded-sm w-full"
-                    required
-                  />
+                  <div className="relative">
+                    <input
+                      id="confirmPassword"
+                      type={showConfirmPassword ? "text" : "password"}
+                      name="confirmPassword"
+                      value={confirmPassword}
+                      onChange={(e) => setConfirmPassword(e.target.value)}
+                      placeholder="••••••••••••••"
+                      className="w-full pr-10 rounded-sm bg-white/90"
+                      required
+                    />
+                    <button
+                      type="button"
+                      onClick={() =>
+                        setShowConfirmPassword(!showConfirmPassword)
+                      }
+                      className="absolute text-gray-600 -translate-y-1/2 right-2 top-1/2 hover:text-gray-800"
+                    >
+                      {showConfirmPassword ? "👁️" : "👁️‍🗨️"}
+                    </button>
+                  </div>
                 </div>
 
                 <button
                   type="submit"
-                  className="col-span-4 col-start-2 w-full bg-amber-500 hover:bg-amber-600 text-white font-medium py-2 rounded-sm "
+                  className="w-full col-span-4 col-start-2 py-2 font-medium text-white rounded-sm bg-amber-500 hover:bg-amber-600 "
                 >
                   Sign up
                 </button>
 
-                <div className="text-center space-y-2 col-span-6">
+                <div className="col-span-6 space-y-2 text-center">
                   <a
                     href="/login"
-                    className="text-white/90 hover:text-white text-sm block"
+                    className="block text-sm text-white/90 hover:text-white"
                   >
                     Already have an account?
                   </a>
-                
                 </div>
               </form>
             </div>
