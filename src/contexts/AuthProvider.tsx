8af76050cc8a2ed66f25531038894231d2b588b7
LeyLopez <leidislopez2017@gmail.com>
2025-05-29 16:21:16 -0500

    Updating itinerary view to see activities

--- src/contexts/AuthProvider.tsx
+++ src/contexts/AuthProvider.tsx
@@ -1,16 +1,27 @@
 import { ReactNode, useState, useEffect, createContext } from "react";
 import { firebaseApp } from "@/firebase/credentials";
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 import { doc, getFirestore, setDoc } from "firebase/firestore";
 import { getIdToken } from "firebase/auth";
 const auth = getAuth(firebaseApp);
 
 export interface AuthContextType {
-    user: any | null;
-    register: (email: string, password: string, firstName: string, lastname: string) => Promise<void>;
-    login: (email: string, password: string) => Promise<void>;
-    logout: () => Promise<void>;
-    getToken: () => Promise<string | null>;
+  user: any | null;
+  register: (
+    email: string,
+    password: string,
+    firstName: string,
+    lastname: string
+  ) => Promise<void>;
+  login: (email: string, password: string) => Promise<void>;
+  logout: () => Promise<void>;
+  getToken: () => Promise<string | null>;
 }
 
 export const AuthContext = createContext<AuthContextType | null>(null);
@@ -32,21 +43,39 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
       } else {
         setUser(null);
         localStorage.removeItem("user");
+        localStorage.removeItem("firebase_token");
       }
     });
     return () => unsubscribe();
   }, []);
 
-  async function register(email: string, password: string, firstName: string, lastname: string) {
+  async function register(
+    email: string,
+    password: string,
+    firstName: string,
+    lastname: string
+  ) {
     const firestore = getFirestore(firebaseApp);
-    const userInfo = await createUserWithEmailAndPassword(auth, email, password);
+    const userInfo = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
     const docRef = doc(firestore, `users/${userInfo.user.uid}`);
-    await setDoc(docRef, { email: email, firstName: firstName, lastname: lastname });
+    await setDoc(docRef, {
+      email: email,
+      firstName: firstName,
+      lastname: lastname,
+    });
   }
 
   async function login(email: string, password: string) {
     await signInWithEmailAndPassword(auth, email, password);
     // El usuario se actualizará automáticamente por onAuthStateChanged
+    const token = await auth.currentUser?.getIdToken();
+    if (token) {
+      localStorage.setItem("firebase_token", token);
+    }
   }
 
   async function logout() {
@@ -56,12 +85,12 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   }
 
   async function getToken(): Promise<string | null> {
-  const currentUser = auth.currentUser;
-  if (currentUser) {
-    return await getIdToken(currentUser);
+    const currentUser = auth.currentUser;
+    if (currentUser) {
+      return await getIdToken(currentUser);
+    }
+    return null;
   }
-  return null;
-}
 
   return (
     <AuthContext.Provider value={{ user, register, login, logout, getToken }}>
--- src/pages/ItineraryHistory.tsx
+++ src/pages/ItineraryHistory.tsx
@@ -4,29 +4,190 @@ import {
   Accordion,
   AccordionDetails,
   AccordionSummary,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
   Typography,
 } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { AuthContext } from "@/contexts/AuthProvider";
+import EditIcon from '@mui/icons-material/Edit';
+
+interface Activity {
+  id: number;
+  hour: string;
+  description: string;
+  state: string;
+}
+
+interface Itinerary {
+  id: string;
+  city: string;
+  creation_date: string;
+  predicted_temperature: string;
+  state: string;
+  activities: Activity[];
+}
+
 
 export const ItineraryHistory = () => {
   const [expanded, setExpanded] = React.useState<string | false>(false);
 
-  const handleChange =
-    (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+  const authContext = React.useContext(AuthContext);
+
+  const handleChange =(panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
       setExpanded(isExpanded ? panel : false);
-    };
+  };
+
+  
+  const getItineraries = (itinerary: Itinerary) => {
+    // Implement the logic to fetch itineraries from the server or state
+    console.log(`Fetching itineraries for ${itinerary.city}`);
+    // This is where you would typically make an API call to get the itineraries
+  }
+    
+  
+  const handleDeleteActivity = (itinerary: Itinerary ,activity: Activity) => {
+    // Implement the logic to delete the activity from the itinerary
+    console.log(`Deleting activity ${activity.id} from itinerary ${itinerary.id}`);
+    // You might want to update the state or make an API call here
+  }
+
+  const handleEditActivity = (itinerary: Itinerary, activity: Activity) => {
+    // Implement the logic to edit the activity in the itinerary
+    console.log(`Editing activity ${activity.id} in itinerary ${itinerary.id}`);
+    // Open a modal to edit the activity
+  }
+
+  // Here we are going to implement the reder of the view when a activity is deleted or edited
+  useEffect(() => {}, []);
+
+
 
   const itineraries = [
-    { id: "panel8", title: "ITINERARIO 8" },
-    { id: "panel7", title: "ITINERARIO 7" },
-    { id: "panel6", title: "ITINERARIO 6" },
-    { id: "panel5", title: "ITINERARIO 5" },
-    { id: "panel4", title: "ITINERARIO 4" },
-    { id: "panel3", title: "ITINERARIO 3" },
-    { id: "panel2", title: "ITINERARIO 2" },
-    { id: "panel1", title: "ITINERARIO 1" },
+    {
+      id: "1",
+      city: "Cartagena",
+      creation_date: "2023-10-01",
+      predicted_temperature: "30°C",
+      state: "Completed",
+      activities: [
+        {
+          id: 2,
+          hour: "10:00",
+          description: "Visita al Castillo San Felipe",
+          state: "Completed",
+        },
+        {
+          id: 3,
+          hour: "12:00",
+          description: "Almuerzo en restaurante local",
+          state: "Completed",
+        },
+        {
+          id: 4,
+          hour: "14:00",
+          description: "Paseo por la ciudad amurallada",
+          state: "Completed",
+        },
+      ],
+    },
+    {
+      id: "2",
+      city: "Leticia",
+      creation_date: "2023-09-15",
+      predicted_temperature: "28°C",
+      state: "Completed",
+      activities: [
+        {
+          id: 3,
+          hour: "10:00",
+          description: "Visita al Parque Nacional Natural Amacayacu",
+          state: "Completed",
+        },
+        {
+          id: 4,
+          hour: "12:00",
+          description: "Almuerzo en restaurante local",
+          state: "Completed",
+        },
+      ],
+    },
+    {
+      id: "3",
+      city: "Santa Marta",
+      creation_date: "2023-08-20",
+      predicted_temperature: "26°C",
+      state: "Completed",
+      activities: [
+        {
+          id: 4,
+          hour: "10:00",
+          description: "Visita al Parque Tayrona",
+          state: "Completed",
+        },
+        {
+          id: 5,
+          hour: "12:00",
+          description: "Almuerzo en restaurante local",
+          state: "Completed",
+        },
+      ],
+    },
+    {
+      id: "4",
+      city: "Medellin",
+      creation_date: "2023-07-10",
+      predicted_temperature: "27°C",
+      state: "Completed",
+      activities: [
+        {
+          id: 5,
+          hour: "10:00",
+          description: "Visita al Parque Arví",
+          state: "Completed",
+        },
+        {
+          id: 6,
+          hour: "12:00",
+          description: "Almuerzo en restaurante local",
+          state: "Completed",
+        },
+      ],
+    },
+    {
+      id: "5",
+      city: "Monteria",
+      creation_date: "2023-06-05",
+      predicted_temperature: "25°C",
+      state: "Completed",
+      activities: [
+        {
+          id: 6,
+          hour: "10:00",
+          description: "Visita al Parque Ronda del Sinú",
+          state: "Completed",
+        },
+        {
+          id: 7,
+          hour: "12:00",
+          description: "Almuerzo en restaurante local",
+          state: "Completed",
+        },
+      ],
+    },
   ];
+
+  const handleLogout = async () => {
+    if (authContext && authContext.user) {
+      await authContext.logout();
+    } else {
+      console.error("AuthContext or logout function is not available.");
+    }
+  };
 
   return (
     <>
@@ -37,7 +198,10 @@ export const ItineraryHistory = () => {
             <h2 className="font-serif text-4xl font-bold text-white">
               ITINERATY HISTORY
             </h2>
-            <button className="px-4 py-2 text-white bg-orange-500 rounded-md hover:bg-orange-600">
+            <button
+              onClick={handleLogout}
+              className="px-4 py-2 text-white bg-orange-500 rounded-md hover:bg-orange-600 "
+            >
               Cerrar sesión
             </button>
           </div>
@@ -84,7 +248,7 @@ export const ItineraryHistory = () => {
                       color: "white",
                     }}
                   >
-                    {itinerary.title}
+                    {itinerary.city} - {itinerary.creation_date}
                   </Typography>
                 </AccordionSummary>
                 <AccordionDetails
@@ -94,9 +258,38 @@ export const ItineraryHistory = () => {
                     padding: "16px",
                   }}
                 >
-                  <Typography sx={{ color: "white" }}>
-                    Detalles del itinerario: fecha, destino, actividades.
-                  </Typography>
+                  <List
+                    
+                  >
+                    {itinerary.activities.map((activity) => (
+                      <ListItem
+                        key={activity.id}
+                        disableGutters
+                        secondaryAction={
+                          <span>
+                            <IconButton aria-label="edit">
+                              <EditIcon fontSize="small" />
+                            </IconButton>
+                            <IconButton aria-label="comment">
+                              <DeleteIcon fontSize="small" />
+                            </IconButton>
+                          </span>
+                        }
+                      >
+                        <ListItemText
+                          primary={
+                            <>
+                              <span>Actividad {activity.id}</span>
+                              <br />
+                              <span>{activity.description}</span>
+                              <br />
+                              <span>Estado: {activity.state}</span>
+                            </>
+                          }
+                        />
+                      </ListItem>
+                    ))}
+                  </List>
                 </AccordionDetails>
               </Accordion>
             ))}
