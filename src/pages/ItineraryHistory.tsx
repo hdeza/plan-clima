0ab8bd4b4f9f877dcdc3ec8823266f9792a5dede
LeyLopez <leidislopez2017@gmail.com>
2025-05-30 00:32:38 -0500

    Update activity div

--- src/pages/ItineraryHistory.tsx
+++ src/pages/ItineraryHistory.tsx
@@ -5,12 +5,16 @@ import {
   AccordionDetails,
   AccordionSummary,
   Alert,
+  Box,
+  Button,
   IconButton,
   List,
   ListItem,
   ListItemText,
+  Modal,
   Snackbar,
   SnackbarCloseReason,
+  TextField,
   Typography,
 } from "@mui/material";
 import React, { useEffect } from "react";
@@ -23,8 +27,6 @@ import { Itinerary } from "@/interfaces/Itinerary";
 import { Activity } from "@/interfaces/activity";
 import { itineraryService } from "@/services/itineraryService";
 
-
-
 export const ItineraryHistory = () => {
   const navigate = useNavigate();
 
@@ -32,7 +34,9 @@ export const ItineraryHistory = () => {
   const [alertMessage, setAlertMessage] = React.useState<string>("");
   const [open, setOpen] = React.useState(false);
   const [itineraries, setItineraries] = React.useState<Itinerary[]>([]);
-  
+  const [openEdit, setOpenEdit] = React.useState(false);
+  const handleOpenEdit = () => setOpenEdit(true);
+  const handleCloseEdit = () => setOpenEdit(false);
 
   const authContext = React.useContext(AuthContext);
 
@@ -41,25 +45,21 @@ export const ItineraryHistory = () => {
       setExpanded(isExpanded ? panel : false);
     };
 
-  const getItineraries = async() => {
-    try{
+  const getItineraries = async () => {
+    try {
       const response = await itineraryService(authContext).getItineraries();
       setItineraries(response);
       console.log("Itineraries fetched successfully:", response);
-      
-    }catch (error) {
+    } catch (error) {
       console.error("Error fetching itineraries:", error);
       setAlertMessage("Failed to fetch itineraries. Please try again.");
       setOpen(true);
     }
-    
   };
 
   const handleDeleteActivity = (activity: Activity) => {
     // Implement the logic to delete the activity from the itinerary
-    console.log(
-      `Deleting activity ${activity.id}`
-    );
+    console.log(`Deleting activity ${activity.id}`);
   };
 
   const handleEditActivity = (activity: Activity) => {
@@ -82,8 +82,7 @@ export const ItineraryHistory = () => {
     };
 
     fetchItineraries();
-  }, []); 
-
+  }, []);
 
   const handleLogout = async () => {
     if (authContext && authContext.user) {
@@ -94,10 +93,9 @@ export const ItineraryHistory = () => {
         setOpen(false);
         setAlertMessage("");
       }, 3000); // Clear alert after 3 seconds
-      
+
       navigate("/login");
       // window.location.href = "/login";
-      
     } else {
       setOpen(true);
       setAlertMessage("Logout failed. Please try again.");
@@ -132,16 +130,14 @@ export const ItineraryHistory = () => {
           state: "Completed",
         },
       ],
-    }
+    },
   ];
-
- 
 
   const handleClose = (
     event?: React.SyntheticEvent | Event,
-    reason?: SnackbarCloseReason,
+    reason?: SnackbarCloseReason
   ) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
 
@@ -167,11 +163,11 @@ export const ItineraryHistory = () => {
 
           <div className="space-y-4 max-h-[60vh] overflow-y-auto pr-2 custom-scrollbar">
             {itineraries.map((itinerary) => (
-              <Accordion
-                key={itinerary.id}
-                expanded={expanded === String(itinerary.id)}
-                onChange={handleChange(String(itinerary.id))}
-                sx={{
+                <Accordion
+                  key={itinerary.id}
+                  expanded={expanded === String(itinerary.id)}
+                  onChange={handleChange(String(itinerary.id))}
+                  sx={{
                   backgroundColor: "rgba(255, 255, 255, 0.2)",
                   backdropFilter: "blur(8px)",
                   border: "2px solid rgba(255, 255, 255, 0.25)",
@@ -186,96 +182,136 @@ export const ItineraryHistory = () => {
                   "&:before": {
                     display: "none",
                   },
-                }}
-              >
-                <AccordionSummary
+                  }}
+                >
+                  <AccordionSummary
                   expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
                   aria-controls={`${itinerary.id}-content`}
                   id={`${itinerary.id}-header`}
                   sx={{
                     "& .MuiAccordionSummary-expandIconWrapper": {
-                      color: "white",
+                    color: "white",
                     },
                   }}
-                >
+                  >
                   <Typography
                     component="span"
                     sx={{
-                      fontFamily: "serif",
-                      fontWeight: "bold",
-                      fontSize: "1.25rem",
-                      color: "white",
+                    fontFamily: "serif",
+                    fontWeight: "bold",
+                    fontSize: "1.25rem",
+                    color: "white",
                     }}
                   >
                     <span>
-                      {itinerary.city} - {itinerary.creation_date instanceof Date ? itinerary.creation_date.toLocaleDateString() : itinerary.creation_date}
+                    {itinerary.city} -{" "}
+                    {itinerary.creation_date instanceof Date
+                      ? itinerary.creation_date.toLocaleDateString()
+                      : itinerary.creation_date}
                     </span>
 
                     <Typography>
-                      <span>
-                        Predicted temperature: {itinerary.predicted_temperature}°C
-                      </span>
+                    <span>
+                      Predicted temperature:{" "}
+                      {itinerary.predicted_temperature}
+                      °C
+                    </span>
 
-                      <br />
-                      <span>Status: {itinerary.state}</span>
+                    <br />
+                    <span>Status: {itinerary.state}</span>
                     </Typography>
                   </Typography>
-                </AccordionSummary>
-                <AccordionDetails
+                  </AccordionSummary>
+                  <AccordionDetails
                   sx={{
                     backgroundColor: "rgba(255, 255, 255, 0.3)",
                     borderTop: "1px solid rgba(255, 255, 255, 0.25)",
                     padding: "16px",
                   }}
-                >
+                  >
                   <List>
                     {itinerary.activities.map((activity) => (
+                    <React.Fragment key={activity.id}>
                       <ListItem
-                        key={activity.id}
-                        disableGutters
-                        secondaryAction={
-                          <span>
-                            <IconButton aria-label="edit">
-                              <EditIcon fontSize="small" />
-                            </IconButton>
-                            <IconButton aria-label="comment">
-                              <DeleteIcon fontSize="small" />
-                            </IconButton>
-                          </span>
-                        }
+                      disableGutters
+                      secondaryAction={
+                        <span>
+                        <IconButton
+                          aria-label="edit"
+                          onClick={() => {
+                          setOpenEdit(activity.id ? true : false);
+                          }}
+                        >
+                          <EditIcon fontSize="small" />
+                        </IconButton>
+                        <IconButton aria-label="comment">
+                          <DeleteIcon fontSize="small" />
+                        </IconButton>
+                        </span>
+                      }
                       >
-                        <ListItemText
-                          primary={
-                            <>
-                              <span>Activity {activity.id}</span>
-                              <br />
-                              <span>{activity.description}</span>
-                              <br />
-                              <span>Status: {activity.state}</span>
-                            </>
-                          }
-                        />
+                      <ListItemText
+                        primary={
+                        <>
+                          <span>Activity {activity.id}</span>
+                          <br />
+                          <span>{activity.description}</span>
+                          <br />
+                          <span>Status: {activity.state}</span>
+                          <br />
+                          <span>Hour: {activity.hour}</span>
+                        </>
+                        }
+                      />
                       </ListItem>
+                      {openEdit  && (
+                      <div className="grid grid-cols-2 gap-4 bg-white/70 p-4 rounded-lg mb-4">
+                        <TextField id="filled-hour" label="Hour" variant="filled" defaultValue={activity.hour} />
+                        <TextField
+                        id="filled-description"
+                        label="Description"
+                        variant="filled"
+                        defaultValue={activity.description}
+                        />
+                        <TextField
+                        id="filled-status"
+                        label="Status"
+                        variant="filled"
+                        defaultValue={activity.state}
+                        />
+                        <br />
+                        <button className="col-start-1 col-end-2 text-cyan-50 bg-orange-500 py-2 rounded-md w-1/2 h-full">
+                        Update activity
+                        </button>
+                        <button
+                        className="text-cyan-50 bg-gray-400 py-2 rounded-md w-1/2 h-full"
+                        onClick={() => setOpenEdit(false)}
+                        >
+                        Cancel
+                        </button>
+                      </div>
+                      )}
+                    </React.Fragment>
                     ))}
                   </List>
-                </AccordionDetails>
-              </Accordion>
+                  </AccordionDetails>
+                </Accordion>
             ))}
           </div>
         </div>
       </div>
       <div>
-      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-        <Alert
-          onClose={handleClose}
-          severity="success"
-          variant="filled"
-          sx={{ width: '100%' }}
-        >
-          {alertMessage}
-        </Alert>
-      </Snackbar>
-    </div>
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert
+            onClose={handleClose}
+            severity="success"
+            variant="filled"
+            sx={{ width: "100%" }}
+          >
+            {alertMessage}
+          </Alert>
+        </Snackbar>
+      </div>
       <Footer />
     </>
   );
